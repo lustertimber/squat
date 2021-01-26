7757fb4e5852385f06c24b2b13316960e68c3988
lustertimber <lustertimber@gmail.com>
2021-01-26 17:47:43 +0900

    Add nodejs Study

--- javascript/nodejs_study_2.js
+++ javascript/nodejs_study_2.js
@@ -1,15 +1,15 @@
 'use strict'; // added ECMAScript 5, 비정상적인 문구에 대한 오류를 Debug console에 출력
 console.log("_> Load completed : nodej_study_1.js");
 
-(async () => {
-    try {
-        const result = await axios.get('http://www.zerocho.com/api/get');
-        console.log(result);
-        console.log(result.data); // {}
-    } catch {
-        console.error(error);
-    }
-})();
+// (async () => {
+//     try {
+//         const result = await axios.get('http://www.zerocho.com/api/get');
+//         console.log(result);
+//         console.log(result.data); // {}
+//     } catch {
+//         console.error(error);
+//     }
+// })();
 
 // (async () => {
 //     try {
@@ -19,4 +19,4 @@ console.log("_> Load completed : nodej_study_1.js");
 //     } catch {
 //         // console.error(error);
 //     }
-// })();
+// })();`
---
+++ nodejsStudy/var.js
@@ -0,0 +1,4 @@
+const odd   = '홍수입니다.';
+const even  = '짝수입니다.';
+
+module.exports(odd,even);
